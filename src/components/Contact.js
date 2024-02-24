1f2eb584252b36789abcdac495a65760b3b37e9d
YJ_ <Pumbaaxx@icloud.com>
2024-02-23 20:07:06 -0800

    integrated UI components

---
+++ src/components/Contact.js
@@ -0,0 +1,16 @@
+export default function Contact() {
+  return (
+    <div className="contact my-6 text-center">
+      <h2 className="text-4xl font-bold mb-4 bg-clip-text text-transparent bg-gradient-to-r from-purple-500 to-pink-500">
+        Interested?
+      </h2>
+      <p className="text-lg text-grey-500 mb-4">Get in touch with us!</p>
+      <button
+        type="button"
+        className="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+      >
+        CONTACT
+      </button>
+    </div>
+  );
+}
---
+++ src/components/main.js
@@ -0,0 +1,65 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+// import DefinitionList from 'DefinitionList';
+import Header from "./Header.js";
+import Footer from "./Footer.js";
+import Contact from "./Contact.js";
+import SearchInput from "./SearchInput.js";
+// import the components in the above section
+
+// main display goes here.
+
+// add in the components and conditionally rendering the content to user
+function Main() {
+  // states declarations to store data between rendering
+  const [word, setWord] = useState("");
+  const [fetchData, setFetchData] = useState([]);
+
+  //Update word useState hook, everytime userinput
+
+  function handleWord(e) {
+    setWord(e.target.value);
+  }
+  // When Search button is click, fetch data from free dictionary api of the word in the form.
+  // Save the definition fetched into fetch data useState hook
+  // or display error if word is not
+
+  const wordSubmit = async (e) => {
+    e.preventDefault();
+    console.log(word);
+    await axios
+      .get("https://api.dictionaryapi.dev/api/v2/entries/en/" + word)
+      .then((res) => {
+        setFetchData(res.data);
+      })
+      .catch((error) => {
+        console.error(
+          "Error fetching from the word: " + word + ".\n Error: ",
+          error
+        );
+        setFetchData({});
+      });
+  };
+  // re-render the page when word definition is updated
+  useEffect(() => {
+    console.log(fetchData);
+  }, [fetchData]);
+
+  return (
+    <>
+      <Header />
+      {/* <input onChange={handleWord} val={word} />
+      <button onClick={wordSubmit}>click here</button> */}
+      <SearchInput
+        value={word}
+        handleWord={handleWord}
+        wordSubmit={wordSubmit}
+      />
+      <Contact />
+      <Footer />
+    </>
+    // <DefinitionList definition={fetchData} />
+  );
+}
+
+export default Main;
