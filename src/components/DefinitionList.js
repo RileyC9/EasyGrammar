1423fed2d3b4fb32a30272e2d2b830db3108c291
Xav92 <xavier.pardanaud@gmail.com>
2024-02-27 12:37:43 -0800

    Updated DefinitionList.js, commented DefinitionList.js

--- src/components/DefinitionList.js
+++ src/components/DefinitionList.js
@@ -4,6 +4,7 @@ import React from "react";
 const Definition = (props) => {
 
 let id = 0;   
+// Going through the dictionary API to get only one definition
     const defList = [];
     props.definition.slice(0,1).forEach(element => element.meanings.forEach((set) => set.definitions.slice(0,1).forEach(definition => {
     defList.push(<li key={id++}>{definition.definition}</li>);
@@ -11,6 +12,7 @@ let id = 0;
     }
     )));
 
+// Going through the dictionary API to get only one phonetic audio
     const speechList = [];
     props.definition.slice(0,1).forEach(element => element.phonetics.slice(0,1).forEach((phonetic)=> {
         speechList.push(<li key={id++}><audio src={phonetic.audio} controls/> </li>);
@@ -19,9 +21,11 @@ let id = 0;
     ));
 return (
     <div>
+{/* displaying the definition */}
         <ul>
             {defList}
         </ul>
+{/* displaying the phonetic audio */}
         <ul>
             {speechList}
         </ul>
