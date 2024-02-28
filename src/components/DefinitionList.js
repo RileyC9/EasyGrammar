d17f7ab0c33f050db22157c95bf24201ee7522fb
Riley Cheung <tcheung25@mylangara.ca>
2024-02-27 21:44:17 -0800

    added comments for GUI implementation. Cleaned up codes.

--- src/components/DefinitionList.js
+++ src/components/DefinitionList.js
@@ -2,39 +2,38 @@ import React from "react";
 
 
 const Definition = (props) => {
-
-let id = 0;   
-// Going through the dictionary API to get only one definition
+    // generate unique id for every rendered element
+    let id = 0;   
+    // Going through the dictionary API to get only one definition
     const defList = [];
     props.definition.slice(0,1).forEach(element => element.meanings.forEach((set) => set.definitions.slice(0,1).forEach(definition => {
-    defList.push(<li key={id++}>{definition.definition}</li>);
-        
-    }
-    )));
+    defList.push(
+        // Yujie, Here generates html elements for every definition
+        <li key={id++}>{definition.definition}</li>
+        );
+    })));
 
-// Going through the dictionary API to get only one phonetic audio
+    // Going through the dictionary API to get only one phonetic audio
     const speechList = [];
     props.definition.slice(0,1).forEach(element => element.phonetics.slice(0,1).forEach((phonetic)=> {
-        speechList.push(<li key={id++}><audio src={phonetic.audio} controls/> </li>);
+        speechList.push(
+            // Yujie, Here generates html elements for every audio
+            <li key={id++}><audio src={phonetic.audio} controls/> </li>
+        );
         
-    }
-    ));
-return (
-    <div>
-{/* displaying the definition */}
-        <ul>
-            {defList}
-        </ul>
-{/* displaying the phonetic audio */}
-        <ul>
-            {speechList}
-        </ul>
+    }));
 
-
-    </div>
-
-
+    return (
+        <div>
+            {/* Yujie, here displays the definition. setup the wrapper if needed */}
+            <ul>
+                {defList}
+            </ul>
+            {/* Yujie, here displays the phonetic audio. setup the wrapper if needed */}
+            <ul>
+                {speechList}
+            </ul>
+        </div>
 )}
-                    
+
 export default Definition; 
-                            
