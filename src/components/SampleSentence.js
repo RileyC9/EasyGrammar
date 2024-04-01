19c54691c9bfdbb733630fc12f5e9ce2bcb3b57d
Riley Cheung <tcheung25@mylangara.ca>
2024-04-01 10:13:04 -0700

    Updated SampleSentence.js: Clean up code and modified comments for documentation.

--- src/components/SampleSentence.js
+++ src/components/SampleSentence.js
@@ -31,11 +31,10 @@ const SampleSentences = ({ data, display }) => {
 
   return (
     <section className="block w-full">
-      {/* display the word and definition if input is valid and no error and exampleList is not empty */}
+      {/* display the sample sentences if input is valid, no error and exampleList is not empty */}
       {display && !error && exampleList.length > 0 && (
         <div className="max-w-4xl mx-auto">
           {/* Sample sentence */}
-          {/* Yujie, please modify the Sample sentence appearance */}
           <ul className="mx-4 p-6 rounded-2xl bg-white shadow-md text-left flex flex-col gap-6 md:mx-auto">
             <div className="-ml-6 -mt-6 mr-auto rounded-tl-2xl rounded-br-2xl bg-purple-100">
               <h2 className="font-semibold p-4">Usage Examples</h2>
