8e94acfa52960dbd3964bc37832f974330ca8f54
RAVINESH SAMI <RSAMI00@MYLANGARA.CA>
2024-04-08 10:56:05 -0700

    created IntegrationTest.test.js and axios.js files---added test cases for Definition List feature and Audio Feature and mock axios for testing

---
+++ src/__mocks__/axios.js
@@ -0,0 +1,28 @@
+export default {
+  get: jest.fn(() => Promise.resolve({ 
+    data: [
+      {
+        "word": "innovation",
+        "phonetic": "/ˌɪnəˈveɪʃən/",
+        "phonetics": [
+          {
+              "text": "/ˌɪnəˈveɪʃən/",
+              "audio": "https://www.example.com/audio.mp3",
+          }
+        ],
+        "meanings": [
+          {
+            "partOfSpeech": "noun",
+            "definitions": [
+              {
+                "definition": "The act of innovating; the introduction of something new, in customs, rites, etc.",
+                "synonyms": [],
+                "antonyms": []
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }))
+}
